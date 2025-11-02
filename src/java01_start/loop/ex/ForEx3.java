a0746dde3e1fae1614891f94eab48e4cae602e84
JungH200000 <jhpaxk02@naver.com>
2025-11-02 21:00:40 +0900

    study: Java 반복문 문제와 풀이

---
+++ src/java01_start/loop/ex/ForEx3.java
@@ -0,0 +1,14 @@
+package java01_start.loop.ex;
+
+public class ForEx3 {
+
+  public static void main(String[] args) {
+    int max = 100;
+
+    int sum = 0;
+    for (int i = 1; i <= max; i++) {
+      sum = sum + i;
+    }
+    System.out.println(sum);
+  }
+}
