4183938d40264510e8e9ebd513e43a9abb034945
JungH200000 <jhpaxk02@naver.com>
2025-11-11 23:11:50 +0900

    study: Java 훈련 - 문제와 풀이2: 수정

--- src/java01_start/scanner/ex/ScannerEx5.java
+++ src/java01_start/scanner/ex/ScannerEx5.java
@@ -10,7 +10,7 @@ public class ScannerEx5 {
 
       System.out.print("첫 번째 숫자를 입력하세요: ");
       int num1 = scanner.nextInt();
-
+ 
       System.out.print("두 번째 숫자를 입력하세요: ");
       int num2 = scanner.nextInt();
 
