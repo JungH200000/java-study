949855dd493a61ad0e0b0fbc35cda7e239c80ba4
Jung20000 <jhpaxk02@naver.com>
2025-11-12 11:08:19 +0900

    study: Java 훈련 - 문제와 풀이3: 주석으로 설명 추가

--- src/java01_start/scanner/ex/ScannerWhileEx1.java
+++ src/java01_start/scanner/ex/ScannerWhileEx1.java
@@ -9,17 +9,18 @@ public class ScannerWhileEx1 {
 
     while (true) {
       System.out.print("이름을 입력하세요 (종료를 입력하면 종료): ");
-      String name = input.nextLine();
+      String name = input.nextLine(); // 예시: 자바\n 입력
+
       if (name.equals("종료")) {
         System.out.println("프로그램을 종료합니다.");
         break;
       }
 
       System.out.print("나이를 입력하세요: ");
-      int age = input.nextInt();
-      // 만약 10을 입력한다면 `10\n`이 저장됨 -> 출력은 10만하고 버퍼에 `\n`이 남아있음
-      // -> 나중에 `nextLine()`을 사용한다면 "이름을 입력하세요 (종료를 입력하면 종료): 나이를 입력하세요:" 이런 형태가 되어버림
-      // -> 그래서 nextInt() 사용 후 nextLine()을 사용해야 한다면 nextInt 사용 후 `input.nextLine()`을 입력해두자.
+      int age = input.nextInt(); // 예시: 10\n 입력
+      // `nextInt()`는 10만 가져가서 출력하고 버퍼에 `\n`이 남아있음
+      // 다음 `input.nextLine()`에서 `\n`을 읽음 -> 빈 문자가 들어가게 됨 ("이름을 입력하세요 (종료를 입력하면 종료): 나이를 입력하세요: ")
+      // -> 그래서 `input.nextInt()` 사용 후 `input.nextLine()`을 사용해서 `\n`을 버려주면 된다.
       input.nextLine();
 
       System.out.println("입력한 이름: " + name + ", 나이: " + age);
